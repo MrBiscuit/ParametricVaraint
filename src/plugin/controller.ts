d8004cf138684c306cc0f592ba6dea1e77cbd069
boybook <fangyizhou1997@qq.com>
2022-05-12 23:57:49 +0800

    MessageHandler utils

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,26 +1,17 @@
+import diff from './utilDiff';
+
 figma.showUI(__html__);
 
-figma.ui.onmessage = (msg) => {
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
+let lastSelection;
 
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
+figma.on('selectionchange', () => {
+    const sel = figma.currentPage.selection[0];
+    if (!sel) return;
+    console.log(sel);
+    if (sel.type === 'COMPONENT') {
+        if (lastSelection) {
+            console.log(diff(sel, lastSelection));
         }
-
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-
-        // This is how figma responds back to the ui
-        figma.ui.postMessage({
-            type: 'create-rectangles',
-            message: `Created ${msg.count} Rectangles`,
-        });
+        lastSelection = sel;
     }
-
-    figma.closePlugin();
-};
+});
