6d12c2ee3f7b13721b043f5fe89f423d8df85b98
boybook <fangyizhou1997@qq.com>
2022-05-21 20:28:56 +0800

    一些严格模式的修改

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,7 +1,7 @@
 import {dispatch, handleEvent} from './codeMessageHandler';
 import {createParametricComponentSet, getPCSFromComponentSetNode} from './utilComponentSetSession';
 import {ParametricComponentSetSession} from './ComponentSetSession';
-import {getParametricComponentSet, getParentComponent} from './helper';
+import {getParametricComponentSet} from './helper';
 
 figma.showUI(__html__, {width: 240, height: 240});
 
@@ -26,15 +26,15 @@ handleEvent('createVariant', (row) => {
     });
 });
 
-handleEvent("updateHeight",(height)=>{
+handleEvent('updateHeight', (height) => {
     figma.ui.resize(240, height);
 });
 
-handleEvent("addOptionConfirm", (data)=> {
-    const sel = figma.currentPage.selection[0]
+handleEvent('addOptionConfirm', (data) => {
+    const sel = figma.currentPage.selection[0];
     let node = session?.getComponentVariantNode(sel.id);
-    node.setVariantProp(node.data.variantRow, data.value)
-})
+    node.setVariantProp(node.data.variantRow, data.value);
+});
 
 figma.on('selectionchange', () => {
     const sel = figma.currentPage.selection[0];
